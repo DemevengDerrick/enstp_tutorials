41e44ad927ac9dd8fcc222915e3f96bb5d957b3f
Demeveng Derrick <demeveng@gmail.com>
2024-08-17 13:33:19 +0200

    updated

--- scripts/expression_conditionelle_if_else.R
+++ scripts/expression_conditionelle_if_else.R
@@ -1,6 +1,51 @@
 ################################################################################
 #                                                                              #
-#                        String Manipulation in R                              #
+#                      Conditional expressions in R                            #
 #                       Author : Derrick Demeveng                              #
 #                                                                              #
 ################################################################################
+
+# if
+a <- 33
+b <- 200
+
+if (b > a) {
+  print("b est sup a")
+}
+
+# else if
+a <- 33
+b <- 33
+
+if (b > a) {
+  print("b est sup a")
+} else if (a == b) {
+  print ("a et b sont égaux")
+}
+
+# else
+a <- 200
+b <- 33
+
+if (b > a) {
+  print("b est sup a")
+} else if (a == b) {
+  print("a et b sont égaux")
+} else {
+  print("a est sup b")
+}
+
+# if imbriqué
+x <- 41
+
+if (x > 10) {
+  print("Sup a 10")
+  if (x > 20) {
+    print("aussi sup a 20!")
+  } else {
+    print("mais pas sup a 20.")
+  }
+} else {
+  print("inf a 10.")
+}
+
